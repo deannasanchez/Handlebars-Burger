ac715ec557a756cf5fee29f0ef4de7dbc3f22ed8
Dee Lynn <deannalguzman@gmail.com>
2019-10-30 13:19:43 -0700

    added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+if (process.env.JAWSDB_URL) {
+connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+    connection = mysql.createConnection
+    ({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "milkshake",
-  database: "burgers_db"
+  database: "cat_db"
 });
+};
 
 // we create a promise so that we can make sure any request waits
 // for connection to be connected before running any command.
